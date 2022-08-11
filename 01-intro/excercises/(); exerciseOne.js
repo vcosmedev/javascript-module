0c395143273676538df84bec23925ebde51d7a92
Víctor Cosme <victorcosmema@gmail.com>
2022-08-10 21:56:07 -0500

    Add corrections exercise one: statements and declarations

--- 01-intro/excercises/(); exerciseOne.js
+++ 01-intro/excercises/(); exerciseOne.js
@@ -4,9 +4,9 @@ let numberOne = 2022;
 let numberTwo = 2021;
 
 if (numberOne > numberTwo) {
-    console.log = 'El número uno ' + numberOne + ' es mayor que el número dos ' + numberTwo +' ';
+    console.log = 'El número uno ' + numberOne + ' es mayor que el número dos ' + numberTwo + ' ';
 } else if (numberOne < numberTwo) {
-    console.log = 'El número dos ' + numberTwo + ' es mayor que el núemero uno ' + numberOne +' ';
+    console.log = 'El número dos ' + numberTwo + ' es mayor que el núemero uno ' + numberOne + ' ';
 } else {
-    console.log = 'El número uno ' + numberOne + ' es igual al número dos ' + numberOne +' ';
+    console.log = 'El número uno ' + numberOne + ' es igual al número dos ' + numberOne + ' ';
 }
