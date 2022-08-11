c6a045abc9f4f1d219dd742dd915a77c5cc9d813
Víctor Cosme <victorcosmema@gmail.com>
2022-08-11 01:25:01 -0500

    Add second way to resolve the exercise

--- 01-intro/excercises/(); exerciseOne.js
+++ 01-intro/excercises/(); exerciseOne.js
@@ -10,3 +10,18 @@ if (numberOne > numberTwo) {
 } else {
     console.log = 'El número uno ' + numberOne + ' es igual al número dos ' + numberOne + ' ';
 }
+
+/*
+
+let numberOne = 2022;
+let numberTwo = 2021;
+
+if (numberOne > numberTwo) {
+  console.log(`El número uno ${numberOne} es mayor que el número dos ${numero2}`);
+} else if (numberTwo > numberOne) {
+  console.log(`El número dos ${numberTwo} es mayor que el número one ${numberOne}`);
+} else {
+  console.log(`El número uno ${numberOne} es igual al número dos ${numero2}`);
+}
+
+*/
---
+++ 02-scope-and-functions/(); functions.js
@@ -0,0 +1,14 @@
+// Declarativa
+
+function myFunction(textOne, textTwo) {
+    console.log(textOne + textOne); // concatenate
+}
+
+
+let myVariableIsAFunction = function() {
+
+}
+
+
+
+
