248e8e7b7114f9181877dbe54d5e90c4efaec358
Víctor Cosme <victorcosmema@gmail.com>
2022-08-12 10:06:04 -0500

    Add scope and functions classes and exercises

--- 02-scope-and-functions/(); functions.js
+++ 02-scope-and-functions/(); functions.js
@@ -1,13 +1,50 @@
-// Declarativa
+// Declarative (textOne, textTwo -> son PARÁMETROS cuando declaramos)
 
 function myFunction(textOne, textTwo) {
-    console.log(textOne + textOne); // concatenate
+    console.log(textOne + '' + textOne); // concatenate
+    console.log(`${textOne}` `${textTwo}`)  // dynamic way to generate strings -> Template Strings
 }
 
+let myName = 'Víctor';
+let mySurname = 'Cosme';
+myFunction(myName, mySurname); // Ejecución de la función (name y surname -> ARGUMENTOS)
 
+
+// Expression
 let myVariableIsAFunction = function() {
 
 }
+
+    // Ejectuar función
+    function multiplicate(numberOne, numberTwo) {
+        let result = numberOne * numberTwo;
+        console.log (`${result}`)
+}
+    // Llamar función
+    multiplicate(10, 5);
+
+
+
+// return
+//numberOne + numberTwo -> Expression
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// arrow function //
+
 
 
 
---
+++ 02-scope-and-functions/(); scope.js
@@ -0,0 +1,23 @@
+// // Global scope
+
+// let globalVariable = 15;
+
+// function greetings() {
+//     // Local scope
+
+//     let localVariable = 10;
+//     console.log(globalVariable, localVariable); // 15 10
+// }
+
+// greetings();
+
+
+// diferencia entre método y función
+
+// método -> le pertenece a un objeto
+
+
+// scope en un arrow function VS scope function
+
+
+
