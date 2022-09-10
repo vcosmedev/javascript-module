088259dccd0ae786d6bd6d8e1fca6ee780d6a691
Víctor Cosme <victorcosmema@gmail.com>
2022-09-09 20:42:51 -0500

    Add first part review class 19

--- 19-review/(); review.js
+++ 19-review/(); review.js
@@ -26,10 +26,61 @@ class Animal {  // Método constructor
     }
 
     eat(feeding) { // Sugar syntax al momento de definir un método dentro de una clase
-        console.log(`Este animal es ${feeding}.`)
+        return `Este animal es ${feeding}.`;
     } // De esta forma se define un método que forma parte de una clase
 }
 
-const dog = new Animal(true, 2, 4, 'white'); // Instancia
-console.log(dog.heart);
-dog.eat('Carnívoro facultativo');
+// Herencia
+
+// class Oviparous extends Animal { // Extends -> Palabra reservada que nos permite 'extender' clases (extensiones de clase) (A esta extensión se le llama cadena de prototipos)
+//     constructor (bornFromEggs) {
+//         super(true, 3, true);
+//         this.bornFromEggs = bornFromEggs; // 'super' -> method // siempre hay que llamar a la función antes de asignarle una extensión, en este caso 'bornFromEggs'
+//     }
+// }
+
+// const dog = new Animal(true, 2, 4, 'white'); // Instancia
+// console.log(dog.heart);
+// let outPut = dog.eat('Carnívoro facultativo');
+// console.log(outPut);
+
+// const bird = new Oviparous(true);
+// console.log(bird);
+
+// const sqirtle = new Oviparous(true);
+// console.log(sqirtle);
+
+// const anotherAnimal = new Oviparous(false);
+// console.log(anotherAnimal);
+
+// CUANDO QUEREMOS TENER LOS OBJETOS COMO LOS QUEREMOS ASIGNAR, ES DECIR, CON PARÁMETROS DIFERENTES, AD HOC, 
+// LO PODEMOS HACER DE ESTA FORMA:
+class Oviparous extends Animal { // Extends -> Palabra reservada que nos permite 'extender' clases (extensiones de clase) (A esta extensión se le llama cadena de prototipos)
+    constructor (heart, eyes, bodyExtremities, color, bornFromEggs) {
+        super(heart, eyes, bodyExtremities, color);
+        this.bornFromEggs = bornFromEggs; // 'super' -> method // siempre hay que llamar a la función antes de asignarle una extensión, en este caso 'bornFromEggs'
+    }
+}
+
+class Reptile extends Oviparous { // Extends -> Palabra reservada que nos permite 'extender' clases (extensiones de clase) (A esta extensión se le llama cadena de prototipos)
+    constructor (heart, eyes, bodyExtremities, color, bornFromEggs, reptilesCrawl) {
+        super(heart, eyes, bodyExtremities, color, bornFromEggs);
+        this.reptilesCrawl = reptilesCrawl; // 'super' -> method // siempre hay que llamar a la función antes de asignarle una extensión, en este caso 'bornFromEggs'
+    }
+}
+
+const anotherAnimal = new Oviparous(true, 2, 4, 'white', true, true);
+console.log(anotherAnimal);
+
+const crocodile = new Reptile (true, 2, 4, 'green', true, true);
+console.log(crocodile);
+
+
+
+
+
+
+// Realizar ejemplos de otros animales (ovíparos, vivíparos).
+
+
+
