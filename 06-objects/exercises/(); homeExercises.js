2d502c05a6e4e675e77126bd1eb1f47000d8ed0f
Víctor Cosme <victorcosmema@gmail.com>
2022-09-12 13:01:33 -0500

    Add exercises 06-object class

--- 06-objects/exercises/(); homeExercises.js
+++ 06-objects/exercises/(); homeExercises.js
@@ -6,16 +6,125 @@ Un automóvil
 Una casa
 Una persona
 Un mentor
-Un Koder
+Un koder
 Una computadora
 Un edificio
 Un teléfono móvil
 Un menú de un restaurante
 
-
-
 Función constructura
 */
+
+let car = {
+    brand: 'Mercedes',
+    color: 'Gris',
+    doors: 3,
+    year: 2021,
+    km: 'New',
+    fuel: 'Petrol',
+    petrol: true,
+    hybrid: false
+};
+
+
+let home = {
+    groundFloor: true,
+    firstFloor: true,
+    attic: false,
+    garage: true,
+    rooms: 4,
+    bathrooms: 2,
+    year: 2020,
+    owner: 'Víctor',
+};
+
+
+let person = {
+    name: 'Ángel',
+    man: true,
+    age: 32,
+    weight: '66 KG',
+    height: '1.65 CM',    
+};
+
+let mentor = {
+    name: 'Aldo',
+    generation: 21,
+    module: 'JavaScript',
+    committed: true,
+    goal: 'Teach JS'
+};
+
+
+let koder = {
+    name: 'Víctor',
+    generation: 21,
+    module: 'JavaScript',
+    committed: true,
+    goal: 'Learn JS'
+};
+
+
+let computer = {
+    brand: 'Apple',
+    model: 'MackBook Pro',
+    inch: 14,
+    M1: true,
+    CPU: '10-Core',
+    memory: '16GB',
+    storage: '516GB',
+    color: 'Space gray',
+    touchID: true,
+    refurbished: false
+};
+
+let building = {
+    year: 1996,
+    floors: 26,
+    height: '114,7 m',
+    type: 'skyscraper',
+    officeBuilding: true,
+    elevator: true,
+    location: 'Paseo de la Castellana, 216, Madrid',
+    architects: ['John Burgee', 'Philip Johnson'],
+}
+
+
+let mobile = {
+    brand: 'Apple',
+    model: 'Iphone 14 Pro',
+    inch: 6.1,
+    M2: true,
+    capacity: '256GB',
+    dynamicIsland: 'true',
+    color: 'Space gray',
+    touchID: false,
+    faceID: true,
+    refurbished: false
+}
+
+
+let menu = {
+    
+    vegetarian: {
+        pizzas: ['Margherita', '4 Fromaggi', 'Farmhouse'],
+        salads: ['Tomato Cucumber', 'Quinoa Tabbouleh', 'Tomato Avocado']
+    },
+    veggan: {
+        tacos: ['Fake Pastor', 'Fake Gringa', 'Fake Milanesa']
+    },
+    other: {
+        meat: ['Pork', 'Beef', 'Chicken', 'Turkey', 'Rabbit'],
+        fish: ['Carp', 'Salmon', 'Tuna']
+    },
+    desserts: ['Pie de limón', 'Brownie', 'Banana bread'],
+    averageTicket: '350MXN',
+    
+};
+
+
+
+
 
 // Cómo se generan los objetos, buscar en mdn
 
@@ -34,11 +143,6 @@ Función constructura
     // Contexto de declaración: arrow-functions
 
     //method -> parse; method -> stringify
-
-
-
-
-
 
 
 
