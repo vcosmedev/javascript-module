3ae34f4c8b2ac07ebdd7303af890cb5fa88e7e89
Víctor Cosme <victorcosmema@gmail.com>
2022-09-12 13:10:38 -0500

    Add notes 06-object class

--- 06-objects/(); objects.js
+++ 06-objects/(); objects.js
@@ -7,7 +7,45 @@ let car = {
     }
 }
 
-// atributos: guardan valores de tipo primitivo
+// Atributos: guardan valores de tipo primitivo
 // métodos => guardan funciones
-// las funciones pueden ser utilizadas para generar objetos. a este tipo de funciones se les denomina 'funciones constructoras'
-// existen métodos de string; ejemplo: split.
+// Las funciones pueden ser utilizadas para generar objetos. a este tipo de funciones se les denomina 'funciones constructoras'
+// Existen métodos de string; ejemplo: split.
+
+function Car() {
+    this.doors = 5;
+    this.color = 'gray',
+    this.electric = false,
+    // metodos son propiedades que almacenan funciones
+    this.speedUp = () => {
+        console.log('Estoy avanzando');
+    } 
+}
+
+
+let miniCooper = new Car();
+console.log(miniCooper);
+
+let string = new String();
+console.log(typeof string);
+
+let fun = car.speedUp;
+
+ function receivingCallback(fun){
+    fun();
+ }
+
+ receivingCallback(fun)
+
+function returnAnotherFunction(){
+    let reference = () => {
+        console.log('Hello!')
+    };
+    return reference
+ }
+
+ let result = returnAnotherFunction()();
+
+ // Revisar concepto de referencias
+
+
